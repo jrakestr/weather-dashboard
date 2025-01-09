c78649caf4b1f23ff0c8be9d9ca7e0f8ebadd02d
jrakestr <127575942+jrakestr@users.noreply.github.com>
2025-01-09 16:49:45 -0600

    Simplify render.yaml configuration

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,29 +1,19 @@
-import express from 'express';
 import dotenv from 'dotenv';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import express from 'express';
 dotenv.config();
 
 // Import the routes
 import routes from './routes/index.js';
 
 const app = express();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 3001;
 
-// Serve static files from the client dist folder using absolute path
-app.use(express.static(path.join(__dirname, '../client/dist')));
-
-// Implement middleware for parsing JSON and urlencoded form data
-app.use(express.json());
+// Serves static files in the entire client's dist folder
+app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
-
-// Implement middleware to connect the routes
+app.use(express.json());
 app.use(routes);
 
 // Start the server on the port
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
